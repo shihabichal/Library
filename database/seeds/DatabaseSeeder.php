cfd7a9acdaaad99f42f351b1d9d0486ff5e7a50e
aldhinya <fadhil.rochman@gmail.com>
2021-04-03 21:02:44 +0700

    Ubah seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminSeeder::class);
-        $this->call(MemberSeeder::class);
+        DB::table('admin')->insert([
+            [
+                'nama_admin' => 'Lintang',
+                'email' => 'lintang@gmail.com',
+                'username' => 'admin1',
+                'nohp' => '08121212',
+                'password' => Hash::make('admin1'),
+            ],
+            [
+                'nama_admin' => 'Shihab',
+                'email' => 'shi@gmail.com',
+                'username' => 'admin2',
+                'nohp' => '1212',
+                'password' => Hash::make('admin2'),
+            ]
+        ]);
+        DB::table('member')->insert([
+            [
+                'nama_member' => 'Member',
+                'alamat' => 'Gresik',
+                'nope' => '0877543',
+                'gender' => 'L',
+                'email' => 'member@perpustakaan.com',
+                'password' => Hash::make('member'),
+            ],
+
+        ]);
     }
 }
